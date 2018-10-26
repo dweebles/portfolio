6a8476d15f632f0c5a2b5f889775442cc49bf57e
Duy <sleepynguyener@gmail.com>
2018-10-26 00:29:44 -0400

    Cleaning out

--- app/scripts/resume/resume-ctrl.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('portfolio')
-  .controller('ResumeCtrl', function ($scope) {
-  	$scope.skills = [];
-  	$scope.personalTraits = [];
-  	$scope.experience = [];
-  });
