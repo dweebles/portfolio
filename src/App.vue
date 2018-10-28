a5a48e760e4fe0f921d92d7b40190c175e90bfb2
Duy <sleepynguyener@gmail.com>
2018-10-28 01:21:53 -0400

    Scaffold

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+    <Menu />
+    <div id="container"></div>
+    <HelloWorld msg="Hello there..."/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Menu from "./components/menu.vue";
+import * as Three from "three";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld,
+    Menu
+  },
+  camera: null,
+  scene: null,
+  renderer: null,
+  mesh: null,
+  methods: {
+    init: function() {
+      let container = document.getElementById('container');
+
+      this.camera = new Three.PerspectiveCamera(70, container.clientWidth/container.clientHeight, 0.01, 10);
+      this.camera.position.z = 1;
+
+      this.scene = new Three.Scene();
+
+      let geometry = new Three.BoxGeometry(0.2, 0.2, 0.2);
+      let material = new Three.MeshNormalMaterial();
+
+      this.mesh = new Three.Mesh(geometry, material);
+      this.scene.add(this.mesh);
+
+      this.renderer = new Three.WebGLRenderer({antialias: true});
+      this.renderer.setSize(container.clientWidth, container.clientHeight);
+      container.appendChild(this.renderer.domElement);
+    },
+    animate: function () {
+      requestAnimationFrame(this.animate);
+        this.mesh.rotation.x += 0.01;
+        this.mesh.rotation.y += 0.02;
+        this.renderer.render(this.scene, this.camera);
+    }
+  },
+  mounted() {
+    this.init();
+    this.animate()
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+#container {
+  position: absolute;
+  x: 0;
+  y: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 0;
+}
+body {
+  background-color: #B7ECFF;
+}
+</style>
---
+++ src/components/menu.vue
@@ -0,0 +1,56 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="#">Duy</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="nav-menu">
+        <li v-for="link in menuItems" v-bind:key="link">
+          <a v-bind:href="link.url">{{link.name}}</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: "Menu",
+    data: function() {
+      return {
+        menuItems: [
+          {
+            name: "About",
+            url: "#"
+          },
+          {
+            name: "Work",
+            url: "#"
+          },
+          {
+            name: "Contact",
+            url: "#"
+          }
+        ]
+      }
+    },
+    props: {
+      // items: Array
+    }
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
+  li {
+    display: inline;
+    padding: 0 10px;
+  }
+  nav {
+    background-color: #B7ECFF;
+  }
+</style>
