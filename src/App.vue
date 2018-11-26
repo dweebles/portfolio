b96ed6efe259bff60ee5c8e0856a736339523874
Duy <sleepynguyener@gmail.com>
2018-11-25 23:38:50 -0500

    changed scene background color

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="app">
-    <Menu />
-    <div id="container"></div>
-    <About msg="Hello there..."/>
-  </div>
+	<div id="app">
+		<Menu />
+		<div id="container"></div>
+		<About msg="Hello there..."/>
+	</div>
 </template>
 
 <script>
@@ -12,64 +12,65 @@ import Menu from "./components/menu.vue";
 import * as Three from "three";
 
 export default {
-  name: "app",
-  components: {
-    About,
-    Menu
-  },
-  camera: null,
-  scene: null,
-  renderer: null,
-  mesh: null,
-  methods: {
-    init: function() {
-      let container = document.getElementById('container');
+	name: "app",
+	components: {
+		About,
+		Menu
+	},
+	camera: null,
+	scene: null,
+	renderer: null,
+	mesh: null,
+	methods: {
+    	init: function() {
+			let container = document.getElementById('container');
 
-      this.camera = new Three.PerspectiveCamera(70, container.clientWidth/container.clientHeight, 0.01, 10);
-      this.camera.position.z = 1;
+			this.camera = new Three.PerspectiveCamera(70, container.clientWidth/container.clientHeight, 0.01, 10);
+			this.camera.position.z = 1;
 
-      this.scene = new Three.Scene();
+			this.scene = new Three.Scene();
+			this.scene.background = new Three.Color(0xb7ecff);
 
-      let geometry = new Three.BoxGeometry(0.2, 0.2, 0.2);
-      let material = new Three.MeshNormalMaterial();
+			let geometry = new Three.BoxGeometry(0.2, 0.2, 0.2);
+			let material = new Three.MeshNormalMaterial();
 
-      this.mesh = new Three.Mesh(geometry, material);
-      this.scene.add(this.mesh);
+			this.mesh = new Three.Mesh(geometry, material);
+			this.scene.add(this.mesh);
 
-      this.renderer = new Three.WebGLRenderer({antialias: true});
-      this.renderer.setSize(container.clientWidth, container.clientHeight);
-      container.appendChild(this.renderer.domElement);
-    },
-    animate: function () {
-      requestAnimationFrame(this.animate);
-        this.mesh.rotation.x += 0.01;
-        this.mesh.rotation.y += 0.02;
-        this.renderer.render(this.scene, this.camera);
-    }
-  },
-  mounted() {
-    this.init();
-    this.animate()
-  }
+			this.renderer = new Three.WebGLRenderer({antialias: true});
+			this.renderer.setSize(container.clientWidth, container.clientHeight);
+			container.appendChild(this.renderer.domElement);
+		},
+    	animate: function () {
+			requestAnimationFrame(this.animate);
+			this.mesh.rotation.x += 0.04;	// TODO track mouse movement
+			this.mesh.rotation.y += 0.08;	// TODO track mouse movement
+			this.renderer.render(this.scene, this.camera);
+    	}
+	},
+	mounted() {
+    	this.init();
+		this.animate();
+	}
 };
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #2c3e50;
 }
 #container {
-  position: absolute;
-  x: 0;
-  y: 0;
-  width: 100%;
-  height: 70%;
-  z-index: 0;
+	position: absolute;
+	x: 0;
+	y: 0;
+	width: 100%;
+	height: 70%;
+	z-index: 0;
 }
 body {
-  background-color: #B7ECFF;
+	background-color: #B7ECFF;
 }
 </style>
--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,15 +1,4 @@
 <template>
-  <!-- <nav class="navbar navbar-expand-lg navbar-light">
-    <a class="navbar-brand" href="#">Duy</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="nav-menu">
-        <li v-for="link in menuItems" v-bind:key="link">
-          <a v-bind:href="link.url" class="underlined underlined--thick">{{link.name}}</a>
-        </li>
-      </ul>
-    </div>
-  </nav> -->
   <nav class="navbar navbar-expand-md">
     <a class="navbar-brand" href="#">Duy</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
@@ -69,7 +58,7 @@
     line-height: 1.2;
     text-decoration: none;
     background-image: linear-gradient(to right, #E6FF30 0, #E6FF30 100%);
-    background-position: 0 1.2em;
+    background-position: 0 -4.2em;
     background-size: 0 100%;
     background-repeat: no-repeat;
     transition: background .5s;
