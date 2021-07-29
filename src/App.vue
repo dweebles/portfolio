9c923817ed2a9c7152e3d63363b6f05c101ca362
Duy <sleepynguyener@gmail.com>
2021-07-29 19:27:10 -0400

    Updates

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <template>
-	<div id="app">
-		<b-nav card-header>
-			<a class="navbar-brand mr-auto p-2" href="#">
-    			<img src="./assets/keyboard.png" width="30" height="30" alt="">
-  			</a>
-		    <b-nav-item class="p-2" to="/work" exact exact-active-class="active">Projects</b-nav-item>
-		    <b-nav-item class="p-2" to="/contact" exact exact-active-class="active">Contact</b-nav-item>
-		</b-nav>
-		<UserCard />
-	</div>
+  <div id="app">
+    <b-nav card-header>
+      <a class="navbar-brand mr-auto p-2" href="#">
+        < DuyNguyen />&nbsp;<span class="cursor">&nbsp;</span></a
+      >
+      <b-nav-item
+        class="p-2 nav-links"
+        to="/work"
+        exact
+        exact-active-class="active"
+        >Projects</b-nav-item
+      >
+      <b-nav-item
+        class="p-2 nav-links"
+        to="/contact"
+        exact
+        exact-active-class="active"
+        >Contact</b-nav-item
+      >
+    </b-nav>
+    <UserCard />
+  </div>
 </template>
 
 <script>
@@ -17,45 +29,113 @@ import Menu from "./components/menu.vue";
 import UserCard from "./components/usercard.vue";
 
 export default {
-	name: "app",
-	components: {
-		UserCard
-	},
-	camera: null,
-	scene: null,
-	renderer: null,
-	mesh: null,
-	methods: {},
-	mounted() {}
+  name: "app",
+  components: {
+    UserCard,
+  },
+  camera: null,
+  scene: null,
+  renderer: null,
+  mesh: null,
+  methods: {},
+  mounted() {},
 };
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@300;400;500&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@300;400;500&display=swap");
 #app {
-	font-family: "Avenir", Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	color: #2c3e50;
-	width: 650px;
-	margin: 34px auto 0px auto;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  width: 650px;
+  margin: 34px auto 0px auto;
 }
 body {
-	background-color: #8BC6EC;
-	background-image: linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%);
+  background-color: #8bc6ec;
+  background-image: linear-gradient(135deg, #8bc6ec 0%, #9599e2 100%);
 }
 html {
-	height: 100%;
+  height: 100%;
 }
 .nav {
-	font-family: 'Barlow Condensed', sans-serif;
-	position: relative;
-	top: 64px;
-	z-index: 1;
-	padding-left: 1rem;
+  font-family: "Barlow Condensed", sans-serif;
+  position: relative;
+  top: 64px;
+  z-index: 1;
+  padding-left: 1rem;
+}
+
+.navbar-brand {
+  font-family: "Barlow Condensed", sans-serif;
+  font-weight: 600;
+  color: #f92672;
 }
 .nav-item a {
-	color: #333;
-	font-weight: 500;
+  color: #ae81ff;
+  font-weight: 500;
+}
+
+.cursor {
+  position: absolute;
+  top: 13px;
+  bottom: 0;
+  right: 75%;
+  width: 80px;
+  height: 24px;
+  background: #272822;
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  border-left: 0.1em solid;
+
+  -webkit-animation: typing 2s steps(8, end) infinite,
+    blinking 1s step-end infinite;
+  animation: typing 2s steps(10, end) infinite, blinking 1s step-end infinite;
+}
+
+@-webkit-keyframes typing {
+  from,
+  to {
+    width: 140px;
+  }
+  25% {
+    width: 0;
+  }
+  50% {
+    width: 0;
+  }
+  75% {
+    width: 140px;
+  }
+}
+
+@keyframes typing {
+  from {
+    width: 140px;
+  }
+  to {
+    width: 0;
+  }
+}
+
+@-webkit-keyframes blinking {
+  from,
+  to {
+    border-color: transparent;
+  }
+  50% {
+    border-color: #f8f8f0;
+  }
+}
+
+@keyframes blinking {
+  from,
+  to {
+    border-color: transparent;
+  }
+  50% {
+    border-color: #f8f8f0;
+  }
 }
 </style>
--- src/components/usercard.vue
+++ src/components/usercard.vue
@@ -1,87 +1,93 @@
 <template>
-	<div id="usercard">
-		<b-card>
-		    <div class="img-wrapper">
-		    	<img src="../assets/duy.jpg" class="animated bounce">
-		    </div>
-		    <h4 class="card-title">Duy Nguyen</h4>
-			<b-card-text>
-				Programmer, Cat dad, Horror movie enthusiast, Nerd living in St Pete FL
-			</b-card-text>
-			 <b-card-body>
-			 	<transition name="slide"
-			 				enter-active-class="animated fadeIn"
-			 				leave-class="animated fadeOut">
-					<router-view></router-view>
-				</transition>
-				<ul class="list-inline">
-					<li><a href=""><img src="../assets/linkedin.png" width="20" height="20" /></a></li>
-					<li><a href=""><img src="../assets/instagram.png" width="20" height="20" /></a></li>
-				</ul>
-			</b-card-body>
-	    </b-card>
-	</div>
+  <div id="usercard">
+    <b-card>
+      <div class="img-wrapper">
+        <img src="../assets/duy.png" class="animated bounce" />
+      </div>
+      <h4 class="card-title">About Me</h4>
+      <b-card-text> Programmer, Cat dad, living in St Pete FL </b-card-text>
+      <b-card-body>
+        <transition
+          name="slide"
+          enter-active-class="animated fadeIn"
+          leave-class="animated fadeOut"
+        >
+          <router-view></router-view>
+        </transition>
+        <ul class="list-inline">
+          <li>
+            <a href=""
+              ><img src="../assets/linkedin.png" width="20" height="20"
+            /></a>
+          </li>
+          <li>
+            <a href=""
+              ><img src="../assets/instagram.png" width="20" height="20"
+            /></a>
+          </li>
+        </ul>
+      </b-card-body>
+    </b-card>
+  </div>
 </template>
 
 <script>
-
 export default {
-	name: "usercard",
-	components: {},
-	camera: null,
-	scene: null,
-	renderer: null,
-	mesh: null,
-	methods: {},
-	mounted() {}
+  name: "usercard",
+  components: {},
+  methods: {},
+  mounted() {},
 };
 </script>
 
-<style>
-	#usercard {
-		font-family: 'Barlow Condensed', sans-serif;
-		width: 650px;
-		min-height: 450px;
-		margin: 5px auto 0px auto;
-	}
-	.img-wrapper {
-		position: relative;
-	}
-	.img-wrapper img {
-		width: 130px;
-		height: 130px;
-		border-radius: 50%;
-		border: 1px solid #666;
-		box-shadow: 0px 0px 10px #999;
-		position: absolute;
-		top: -50px;
-		left: 39%;
-	}
-	.card {
-		/*border: 1px solid #999;*/
-		box-shadow: 0px 0px 20px #999;
-	}
-	h4.card-title {
-		margin: 100px auto 0px auto;
-		text-align: center;
-	}
-	.card-body {
-		margin-top: 20px;
-		min-height: 250px;
-		max-height: 450px;
-	}
-	.card-text {
-		margin: 20px auto 0px auto;
-		text-align: center;
-	}
-	.list-inline {
-		text-align: center;
-		position: absolute;
-		bottom: 0px;
-		left: 280px;
-	}
-	.list-inline li {
-		display: inline;
-		padding: .5rem;
-	}
+<style scoped>
+#usercard {
+  font-family: "Barlow Condensed", sans-serif;
+  width: 650px;
+  min-height: 450px;
+  margin: 5px auto 0px auto;
+}
+.img-wrapper {
+  position: relative;
+}
+.img-wrapper img {
+  width: 130px;
+  height: 130px;
+  border-radius: 50%;
+  border: 1px solid #666;
+  /* box-shadow: 0px 0px 10px #999; */
+  position: absolute;
+  top: -50px;
+  left: 39%;
+}
+.card {
+  /*border: 1px solid #999;*/
+  box-shadow: 0px 0px 20px #999;
+  background: #272822;
+}
+h4.card-title {
+  margin: 100px auto 0px auto;
+  text-align: center;
+  color: #fd971f;
+}
+.card-body {
+  margin-top: 20px;
+  min-height: 250px;
+  max-height: 450px;
+}
+.card-text {
+  margin: 20px auto 0px auto;
+  text-align: center;
+  color: #e6db74;
+}
+.list-inline {
+  text-align: center;
+  position: absolute;
+  bottom: 0px;
+  left: 280px;
+}
+.list-inline li {
+  display: inline;
+  padding: 0.5rem;
+}
 </style>
