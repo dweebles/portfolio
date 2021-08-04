af281da1ce358efd530cc5984bea877433872a97
Duy Nguyen <sleepynguyener@gmail.com>
2021-08-04 08:57:53 -0400

    latest

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -5,19 +5,16 @@
         <b>Languages:</b>
         <p>
           <span v-for="language in languages" :key="language">
-            {{ language }}, 
+            {{ language }},
           </span>
         </p>
         <b>Latest projects:</b>
         <p>
-          <span v-for="repo in repos" :key="repo.name">
-            {{ repo.name }}, 
-          </span>
+          <span v-for="repo in repos" :key="repo.name"> {{ repo.name }}, </span>
         </p>
       </b-col>
       <b-col sm="12" md="8">
-        <Bar :chart-data="repoCommitCounts" />
-        <Linechart />
+        <D3LineChart :config="chart_config" :datum="chart_data"></D3LineChart>
       </b-col>
     </b-row>
   </div>
@@ -25,20 +22,55 @@
 
 <script>
 import axios from "axios";
-// import { Bar } from 'vue-chartjs'
-import Linechart from './BarChart.vue'
+import { D3LineChart } from "vue-d3-charts";
 
 export default {
   name: "Work",
   components: {
-    Linechart
+    D3LineChart,
   },
   data() {
     return {
       repos: [],
       languages: [],
       commits: [],
-      repoCommitCounts: []
+      repoCommitCounts: [],
+      chart_data: [
+        { hours: 238, production: 134, date: 2000 },
+        { hours: 938, production: 478, date: 2001 },
+        { hours: 1832, production: 1392, date: 2002 },
+        { hours: 2092, production: 2343, date: 2003 },
+        { hours: 2847, production: 2346, date: 2004 },
+        { hours: 2576, production: 2233, date: 2005 },
+        { hours: 2524, production: 2325, date: 2006 },
+        { hours: 1648, production: 2456, date: 2007 },
+        { hours: 2479, production: 2329, date: 2008 },
+        { hours: 3200, production: 2438, date: 2009 },
+      ],
+      chart_config: {
+        values: ["hours", "production"],
+        color: {
+          scheme: "schemeSet2",
+        },
+        date: {
+          key: "date",
+          inputFormat: "%Y",
+          outputFormat: "%Y",
+        },
+        axis: {
+          yTicks: 3,
+        },
+        points: {
+          visibleSize: 3,
+          hoverSize: 6,
+        },
+        margin: {
+          top: 0,
+          right: 0,
+          bottom: 130,
+          left: 0,
+        },
+      },
     };
   },
   methods: {},
@@ -84,7 +116,7 @@ export default {
         this.repoCommitCounts = repoArr;
       });
 
-      this.renderChart(this.chartData, {});
+    this.renderChart(this.chartData, {});
 
     // await this.renderRepoChart(this.repoCommitCounts);
   },
@@ -97,26 +129,17 @@ export default {
 }
 
 .work b {
-  color: #F8F8F2;
+  color: #f8f8f2;
 }
 
 .work span {
-  color: #A6E22E;
+  color: #a6e22e;
 }
 
-.repo-charts rect {
-  fill: #9effff;
-}
-
-.repo-charts text {
-  fill: white;
-  font: 10px sans-serif;
-  text-anchor: end;
-}
-
-.x-axis text {
-  /* Translate and rotate labels so they fit below bars better. */
-  -moz-transform: rotate(-45) !important;
-  transform: rotate(-45) !important;
+svg.chart {
+  position: relative;
+  bottom: 43px;
+  width: 100%;
+  height: 248px;
 }
 </style>
